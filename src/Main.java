ab062acacc098198d8c97a7e403c7e08aff5910c
Igor Mitin <igor.mitin19969@gmail.com>
2025-05-19 14:34:00 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Netology");
+        System.out.println(2+9);
+
+        int speed=20;
+        System.out.println("Max скорость " + speed + " км/ч!" );
+
+    }
+
+}
